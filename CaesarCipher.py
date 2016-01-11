50c2ef689c285533bae2bcc8ac28c2def1974a7b
Isabel Lee <ijl0322@gmail.com>
2016-01-11 12:17:10 +0800

    retains upper/lower case and punctuations

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,9 +1,14 @@
 import string
 def caesarCipher(shift, word):
-    
+    encode = ""
     for char in word:
-        
-        print chr((ord(char)+shift-96)%26 + 96)
+        if char in string.ascii_lowercase:
+            encode += chr((ord(char)+shift-96)%26 + 96)
+        elif char in string.ascii_uppercase:
+            encode += chr((ord(char)+shift-64)%26 + 64)
+        else:
+            encode += char
+    return encode
     
-caesarCipher(1,"abcdez")
-print string.ascii_lowercase
+print caesarCipher(1,"A cat is a cat ? ")
+
