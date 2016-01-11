19189d95202181518712f21e34d422d69730e8fe
Isabel Lee <ijl0322@gmail.com>
2016-01-11 12:26:19 +0800

    add decoder

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -3,12 +3,25 @@ def caesarCipher(shift, word):
     encode = ""
     for char in word:
         if char in string.ascii_lowercase:
-            encode += chr((ord(char)+shift-96)%26 + 96)
+            encode += chr((ord(char)+shift-97)%26 + 97)
         elif char in string.ascii_uppercase:
-            encode += chr((ord(char)+shift-64)%26 + 64)
+            encode += chr((ord(char)+shift-65)%26 + 65)
         else:
             encode += char
     return encode
     
-print caesarCipher(1,"A cat is a cat ? ")
+print caesarCipher(1,"A cat is a zebra ? ")
 
+def decodeCaesar(shift, word):
+    decode = ""
+    for char in word:
+        if char in string.ascii_lowercase: 
+            decode += chr((ord(char) - shift - 97 + 26)%26 + 97)
+    return decode
+    
+print decodeCaesar(1, "B dbu jt b afcsb ?")
+            
+            
+            
+            
+            
